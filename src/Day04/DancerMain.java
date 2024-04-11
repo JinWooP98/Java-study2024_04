83550862110959ad715251a3ff432eb16d6e20e4
JinWooP98 <qwdk0406@naver.com>
2024-04-11 12:00:23 +0900

    # filed and local 학습

---
+++ src/Day04/DancerMain.java
@@ -0,0 +1,45 @@
+package Day04;
+
+public class DancerMain {
+
+    // 자바의 클래스는 실행용 클래스와 설계용 클래스가 나뉘어져 있음
+    // 실행용 클래스 : 객체를 생성해서 객체의 기능을 실행시키는 곳
+    // --> main이 필요
+    public static void main(String[] args) {
+
+        // 설계도(클래스)를 통해 객체를 찍어냄(생성)
+        Dancer kim = new Dancer();
+
+        // 객체의 속성 부여
+        kim.dancerName = "김철수";
+        kim.crewName = "치즈치즈";
+        kim.genre = "스트릿";
+        kim.danceLevel = 1;
+
+        kim.introduce();
+        kim.dance();
+
+        Dancer park = new Dancer();
+
+        park.dancerName = "박철수";
+        park.crewName = "전쟁이야";
+        park.genre = "코레오";
+        park.danceLevel = 2;
+
+        park.dance();
+        // 생성자 호출
+        Dancer hong = new Dancer();
+        hong.dance();
+
+        Dancer choi = new Dancer("최폭풍");
+        choi.dance();
+        choi.introduce();
+
+        Dancer nanana = new Dancer("냐냐냥", "시골크루");
+        nanana.dance();
+        nanana.introduce();
+
+        Dancer rock = new  Dancer("메로롱", "폭식맨", "락킹");
+        rock.introduce();
+    }
+}
---
+++ src/Day04/FieldAndLocal.java
@@ -0,0 +1,18 @@
+package Day04;
+
+public class FieldAndLocal {
+    // 필드는 값을 초기화 하지 않아도
+    // 각 타입의 기본값으로 자동 세팅
+    // 정수 : 0, 실수 : 0.0, 논리 : false, char : '', 나머지 : null
+    int aaa; // in Heap
+
+    // ccc: 매개 변수(parameter) - 메서드 호출시 반드시 필요한 인자값
+    // 지역변수에 가까움
+    void foo(int ccc) {
+        int bbb = 20; // in stack // 영역이 좁아 쓸거면 쓰고, 아님 쓰지 마 자동으로 값 할당 X
+
+        System.out.println("aaa = " + aaa);
+        System.out.println("bbb = " + bbb);
+        System.out.println("ccc = " + ccc);
+    }
+}
