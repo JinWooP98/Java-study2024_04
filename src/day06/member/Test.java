c9f985b42876dcb06b530e42df508bd6b3ea6a35
JinWooP98 <qwdk0406@naver.com>
2024-04-16 09:56:07 +0900

    # 회원관리 시스템 - 회원정보 객체 설계

---
+++ src/day06/member/Test.java
@@ -0,0 +1,22 @@
+package day06.member;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        Member[] arr = new Member[5];
+
+        Member m1 = new Member("abc@def.com", "1234", "김철수", "남성", 30);
+        Member m2 = new Member("xyz@ghi.com", "4567", "김영희", "여성", 31);
+
+        arr[0] = m1;
+        arr[1] = m2;
+        arr[2] = new Member("ad", "ad", "d", "d", 12);
+
+        System.out.println("m1 = " + m1/*.toString이 생략되어 있음*/);
+        System.out.println("m2 = " + m2);
+
+        Member[] members = {m1, m2};
+
+    }
+}
